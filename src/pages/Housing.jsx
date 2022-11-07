94f8e268921298de9fc9920131e9ec2d6dafd802
Mvtteo <en3zia@gmail.com>
2022-11-07 06:27:42 +0100

    loader added when data is loading

--- src/pages/Housing.jsx
+++ src/pages/Housing.jsx
@@ -1,8 +1,25 @@
 import './css/housing.css'
 import Slider from '../components/Slider/Slider'
 import HousingDisplay from '../components/Housing/logement'
+import { useFetch } from '../utils/hooks/Hook'
 
 function Housing() {
+  const { isLoading } = useFetch(`data.json`)
+  console.log(isLoading)
+  if (isLoading) {
+    return <div className="loader">
+    <div className="lds-roller">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  </div>
+  }
   return (
     <div className="body">
       {/* importation du slider d'images et de la fonction permettant
